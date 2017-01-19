9b42cc36bac246fcc9a335c3f8d0ab6463727f98
nkane312 <nkane312@gmail.com>
2017-01-19 16:52:51 -0600

    updates the grid.component.ts: cellToggle function created, gridButton2 function and html button created, pathFunction started, redefines variables and simplifies rect and text creation, added background image variables/elements and creates path. Updates to grid.component.css and added grid.css to assets adding styling for toggled classes. Adds image to assets folder

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -16,11 +16,19 @@ export class GridComponent {
     private rectSize = 50;
     private gridCols;
     private rectSpacing;
+    private svgContainer;
     private g;
     private svg;
     private htmlElement;
+    private image;
+    private backgroundImage;
+    private imageLink = '../assets/death-star.jpg';
+    private lineFunction;
 
-    private cells;
+
+    private cells:Array<any> = [];
+    private tempRects: any;
+    private tempValues: any;
 
     constructor(private http: Http, private el: ElementRef) {
         this.htmlElement = this.el.nativeElement;
@@ -32,7 +40,16 @@ export class GridComponent {
                 this.gridCols = this.cellData.length / 5;
                 this.rectSpacing = this.rectSize;
                 this.initGrid();
+                this.tempRects._groups[0].forEach((r, i) => {
+                    this.cells.push({rect: r});
+                    
+                });
+                this.tempValues._groups[0].forEach((v, i) => {
+                    this.cells[i].value = v;
+                });
+
             });
+        console.log(this.cells);
     }
 
     private extractData(res: Response) {
@@ -40,12 +57,47 @@ export class GridComponent {
         return body || {};
     }
 
+    
     private initGrid() {
-        this.svg = d3.select(this.htmlElement).append('svg')
-            .attr('preserveAspectRatio', 'xMinYMin meet')
-            .attr('viewBox', '0 0 700 500');
-        this.g = this.svg.append('g');
-        this.cells = this.g.selectAll('.data')
+        
+        this.svgContainer = d3.select('div')
+            .style('width', '50%')
+            .style('padding-bottom','100%')
+            .style('overflow', 'hidden')
+            .style('position', 'relative');
+        this.svg = this.svgContainer.append('svg')
+            .attr('preserveAspectRatio', 'xMaxYMax meet')
+            .attr('viewBox', '0 0 285 300')
+            .style('position', 'absolute')
+            .style('top', 0)
+            .style('left', 0);
+        this.image = d3.select('svg').append('svg:defs')
+            .append('svg:pattern')
+            .attr('id', 'image')
+            .attr('x', this.gridPadding)
+            .attr('y', this.gridPadding)
+            .attr('patternUnits', 'userSpaceOnUse')
+            .attr('height', this.gridCols * this.rectSize)
+            .attr('width', this.gridCols * this.rectSize)
+            .append('svg:image')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('height', this.gridCols * this.rectSize)
+            .attr('width', this.gridCols * this.rectSize)
+            .attr('xlink:href', this.imageLink);
+        var self = this;
+        this.backgroundImage = d3.select('svg').append('path')
+            //.attr('d', (d) => {
+            //    self.pathFunction(this.cellData);
+            //})
+            .attr('d', 'M 25 25, L 25 275, L 275 275, L 275 25 z')
+            .attr('fill', 'url(#image)');
+        this.backgroundImage.transition().attrTween('transform', function(d, i, a) {
+            return d3.interpolateString(a, 'scale(1)');
+        });
+        this.g = this.svg.append('g')
+        
+        this.tempRects = this.g.selectAll('.data')
             .data(this.cellData)
             .enter()
             .append('rect')
@@ -57,20 +109,14 @@ export class GridComponent {
                 .attr('y',(d, i) => {
                     return Math.floor(i / this.gridCols) * this.rectSpacing + this.gridPadding;
                 })
-                .style('fill', '#a61d26')
                 .classed('open', true)
+                .classed('cell', true)
+                .attr('id', function(d, i) { return 'rect' + (i + 1) })
                 .text((d, i) => {
                     return this.cellData.dollarValue;
                 })
-                .on('click', function(d, i) {
-                    d3.select(this)
-                        .style('fill', '#42efef')
-                        .classed('selected', (d, i) => {
-                            return !d3.select(this).classed('selected');
-                        })
-                        .classed('open', (d, i) => {
-                            return !d3.select(this).classed('open');
-                        });
+                .on('click', (d, i) => {
+                    self.cellToggle(this.cells[i]);
                 })
                 .on('mouseover', function(d) {
                     d3.select(this).style('cursor', 'pointer');
@@ -78,7 +124,8 @@ export class GridComponent {
                 .on('mouseout', function(d) {
                     d3.select(this).style('cursor', '');
                 });
-        this.g.selectAll('.values')
+        
+        this.tempValues = this.g.selectAll('.values')
             .data(this.cellData)
             .enter()
             .append('text')
@@ -88,21 +135,109 @@ export class GridComponent {
                 .attr("y", (d, i) => {
                         return Math.floor(i / this.gridCols) * this.rectSpacing + this.gridPadding * 2.25;
                     })
+                .attr('id', function(d, i) { return 'text' + (i + 1) })
+                .classed('open', true)
+                .classed('text', true)
                 .style('font-size', '18px')
                 .style('z-index', '999999999')
                 .style('stroke-width', 0.25)
                 .style('stroke', '#505050')
-                .style('fill', '#fff')
                 .style('text-anchor', 'middle')
                 .style('height', 0)
                 .style('overflow', 'visible')
                 .style('pointer-events', 'none')
                 .style('background', 'none')
                 .text((d, i) => {
-                    console.log(this.cellData[i].dollarValue);
                     return this.cellData[i].dollarValue;
                 }); 
     }
     ngOnInit() {
+
     }
+    /*gridButton(Id) {
+        var rectId = document.getElementById('rect' + Id);
+        rectId.style.fill = 'transparent';
+        var textId = document.getElementById('text' + Id);
+        textId.style.display = 'none';
+    }*/
+    cellToggle(cell){
+        d3.select(cell.rect)
+            .classed('selected', (d, i) => {
+                return !d3.select(cell.rect).classed('selected');
+            })
+            .classed('open', (d, i) => {
+                return !d3.select(cell.rect).classed('open');
+            });
+        d3.select(cell.value)
+            .classed('selectedText', (d, i) => {
+                return !d3.select(cell.value).classed('selectedText');
+            })
+            .classed('open', (d, i) => {
+                return !d3.select(cell.value).classed('open');
+            });
+        console.log
+        console.log(cell.rect);
+        console.log(cell.value);
+        
+    }
+    gridButton2() {
+        var x = document.getElementsByClassName('selected');
+        //console.log(x);
+        var y = document.getElementsByClassName('selectedText');
+        //console.log(y);
+        var selectedArray = Array.prototype.slice.call(x);
+        //console.log(selectedArray);
+        var selectedTextArray = Array.prototype.slice.call(y);
+        //console.log(selectedTextArray);
+        var i;
+        for (i = 0; i < x.length; i++){
+            selectedArray[i].style.fill = 'transparent';
+            selectedTextArray[i].style.display = 'none';
+        }
+    }
+    pathFunction(pathData) {
+        d3.area()
+            .x0(this.gridPadding)
+            .x1(function(d) { return this.gridCols * this.rectSize; })
+            .y0(this.gridPadding)
+            .y1(function(d) { return this.gridCols * this.rectSize; }) 
+    }
+
+    /*update() {
+        var self = this;
+        this.cells = this.g.selectAll('.data')
+        .data(this.cellData)
+        .enter()
+        .append('rect')
+            .attr('height', this.rectSize)
+            .attr('width', this.rectSize)
+            .attr('x', (d, i) => {
+                return (this.rectSpacing * (i % this.gridCols)) + this.gridPadding;
+            })
+            .attr('y',(d, i) => {
+                return Math.floor(i / this.gridCols) * this.rectSpacing + this.gridPadding;
+            })
+            .style('fill', '#a61d26')
+            .classed('open', true)
+            .attr('id', function(d, i) { return 'rect' + (i + 1) })
+            .text((d, i) => {
+                return this.cellData.dollarValue;
+            })
+            .on('click', function(d, i) {
+                self.cellToggle(i);
+
+            })
+            .on('mouseover', function(d) {
+                d3.select(this).style('cursor', 'pointer');
+            })
+            .on('mouseout', function(d) {
+                d3.select(this).style('cursor', '');
+            })
+            .on("click", function(e, i){
+                this.cellData.splice(i, 1);
+                this.update();
+        });
+    this.cells.exit()
+    }*/
 }
+
