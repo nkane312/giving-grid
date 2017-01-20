5b729507d0ff8c51f404c3b1eda82445387fddd0
nkane312 <nkane312@gmail.com>
2017-01-20 17:01:02 -0600

    adds functions for createImage, fillHeight and window.onresize function in constructor, removed some console.logs, added some css styling for button

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -12,8 +12,8 @@ import 'rxjs/Rx';
 export class GridComponent {
     private cellData;
     private gridData;
-    private cellPadding = 25;
-    private rectSize = 50;
+    private textPadding;
+    private rectSize;
     private gridCols;
     private rectSpacing;
     private svgContainer;
@@ -23,9 +23,7 @@ export class GridComponent {
     private image;
     private backgroundImage;
     private imageLink = '../assets/death-star.jpg';
-    private lineFunction;
-    private max_x = 0;
-    private max_y = 0;
+    private extraHeight;
     
 
     private cells:Array<any> = [];
@@ -37,14 +35,20 @@ export class GridComponent {
         this.http.get('/assets/data.json')
             .map(this.extractData)
             .subscribe(data => {
+                window.onresize = () => {
+                    this.rectSize = this.fillHeight(window.innerHeight, 5);
+                    this.textPadding = this.rectSize / 2;
+                    this.initGrid();
+                };
                 this.gridData = data[0];
                 this.cellData = this.gridData.grid.cells;
                 this.gridCols = this.cellData.length / 5;
+                this.rectSize = this.fillHeight(window.innerHeight, 5);
                 this.rectSpacing = this.rectSize;
+                this.textPadding = this.rectSize / 2;
                 this.initGrid();
                 this.tempRects._groups[0].forEach((r, i) => {
                     this.cells.push({rect: r});
-                    
                 });
                 this.tempValues._groups[0].forEach((v, i) => {
                     this.cells[i].value = v;
@@ -65,32 +69,13 @@ export class GridComponent {
             .style('padding-bottom','100%')
             .style('overflow', 'hidden')
             .style('position', 'relative');*/
-        console.log(window.innerWidth);
         this.svg = this.setArea(this.cellData, 'div');
-        this.image = d3.select('svg').append('svg:defs')
-            .append('svg:pattern')
-            .attr('id', 'image')
-            .attr('x', 0)
-            .attr('y', 0)
-            .attr('patternUnits', 'userSpaceOnUse')
-            .attr('height', this.gridCols * this.rectSize)
-            .attr('width', this.gridCols * this.rectSize)
-            .append('svg:image')
-            .attr('x', 0)
-            .attr('y', 0)
-            .attr('height', this.gridCols * this.rectSize)
-            .attr('width', this.gridCols * this.rectSize)
-            .attr('xlink:href', this.imageLink);
+        this.image = this.createImage(this.gridCols, this.rectSize, this.imageLink);
         var self = this;
-        this.backgroundImage = d3.select('svg').append('path')
-            //.attr('d', (d) => {
-            //    self.pathFunction(this.cellData);
-            //})
-            .attr('d', 'M 0 0, L 0 250, L 250 250, L 250 0 z')
-            .attr('fill', 'url(#image)');
-        this.backgroundImage.transition().attrTween('transform', function(d, i, a) {
+        this.backgroundImage = this.createPath(this.gridCols, this.rectSize);
+        /*this.backgroundImage.transition().attrTween('transform', function(d, i, a) {
             return d3.interpolateString(a, 'scale(1)');
-        });
+        });*/
         this.g = this.svg.append('g');
         
         this.tempRects = this.g.selectAll('.data')
@@ -126,15 +111,15 @@ export class GridComponent {
             .enter()
             .append('text')
                 .attr("x", (d, i) => {
-                        return (this.rectSpacing * (i % this.gridCols)) + this.cellPadding;
+                        return (this.rectSpacing * (i % this.gridCols)) + this.textPadding;
                     })
                 .attr("y", (d, i) => {
-                return Math.floor(i / this.gridCols) * this.rectSpacing + this.cellPadding * 1.25;
+                return Math.floor(i / this.gridCols) * this.rectSpacing + this.textPadding * 1.25;
                     })
                 .attr('id', function(d, i) { return 'text' + (i + 1) })
                 .classed('open', true)
                 .classed('text', true)
-                .style('font-size', '18px')
+                .style('font-size', this.textPadding)
                 .style('z-index', '999999999')
                 .style('stroke-width', 0.25)
                 .style('stroke', '#505050')
@@ -151,12 +136,6 @@ export class GridComponent {
     ngOnInit() {
         
     }
-    /*gridButton(Id) {
-        var rectId = document.getElementById('rect' + Id);
-        rectId.style.fill = 'transparent';
-        var textId = document.getElementById('text' + Id);
-        textId.style.display = 'none';
-    }*/
     cellToggle(cell){
         d3.select(cell.rect)
             .classed('selected', (d, i) => {
@@ -184,32 +163,42 @@ export class GridComponent {
             selectedTextArray[i].style.display = 'none';
         }
     }
-    pathFunction(pathData) {
-        d3.area()
-            .x0(this.cellPadding)
-            .x1(function(d) { return this.gridCols * this.rectSize; })
-            .y0(this.cellPadding)
-            .y1(function(d) { return this.gridCols * this.rectSize; }) 
+    createImage(gridCols, rectSize, imageLink) {
+        return d3.select('svg').append('svg:defs')
+            .append('svg:pattern')
+            .attr('id', 'image')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('patternUnits', 'userSpaceOnUse')
+            .attr('height', gridCols * rectSize)
+            .attr('width', gridCols * rectSize)
+            .append('svg:image')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('height', gridCols * rectSize)
+            .attr('width', gridCols * rectSize)
+            .attr('xlink:href', imageLink);
+    }
+    createPath(gridCols, rectSize) {
+        return d3.select('svg').append('path')
+        .attr('d', `M 0 0, L 0 ${gridCols * rectSize}, L ${gridCols * rectSize} ${gridCols * rectSize}, L ${gridCols * rectSize} 0 z`)
+        .attr('fill', 'url(#image)'); 
     }
     setArea(areaData, container) {
-        /*for (var i = 0; i < areaData.length; i++) {
-            var temp_x, temp_y;
-            var temp_x = areaData[i].x_axis + areaData[i].width;
-            var temp_y = areaData[i].y_axis + areaData[i].height;
-
-            if ( temp_x >= this.max_x ) { this.max_x = temp_x; }
-
-            if ( temp_y >= this.max_y ) { this.max_y = temp_y; }
-        }*/
+        //this.extraHeight = window.outerHeight - window.innerHeight;
         return d3.select(container).append('svg')
             .attr('width', window.innerWidth)
             .attr('height', window.innerHeight)
             .attr('preserveAspectRatio', 'xMinYMin meet')
-            .attr('viewBox', '0 0 500 275')
+            .attr('viewBox', `0 0 ${window.innerWidth} ${window.innerHeight}`)
             .style('position', 'absolute')
             .style('top', 0)
             .style('left', 0);
     }
+    fillHeight(height, squares) {
+        return height / squares
+    }
+
     /*update() {
         var self = this;
         this.cells = this.g.selectAll('.data')
@@ -219,10 +208,10 @@ export class GridComponent {
             .attr('height', this.rectSize)
             .attr('width', this.rectSize)
             .attr('x', (d, i) => {
-                return (this.rectSpacing * (i % this.gridCols)) + this.cellPadding;
+                return (this.rectSpacing * (i % this.gridCols)) + this.textPadding;
             })
             .attr('y',(d, i) => {
-                return Math.floor(i / this.gridCols) * this.rectSpacing + this.cellPadding;
+                return Math.floor(i / this.gridCols) * this.rectSpacing + this.textPadding;
             })
             .style('fill', '#a61d26')
             .classed('open', true)
