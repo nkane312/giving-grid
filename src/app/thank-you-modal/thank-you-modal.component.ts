dfb29fe46dd7037ab59845d8cacf002f288798d1
nkane312 <nkane312@gmail.com>
2017-02-06 14:25:29 -0600

    adds animation to the ty modal

--- src/app/thank-you-modal/thank-you-modal.component.ts
+++ src/app/thank-you-modal/thank-you-modal.component.ts
@@ -1,9 +1,31 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, trigger, state, style, animate, transition } from '@angular/core';
 
 @Component({
   selector: 'thank-you-modal',
   templateUrl: './thank-you-modal.component.html',
-  styleUrls: ['./thank-you-modal.component.css']
+  styleUrls: ['./thank-you-modal.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({transform: 'translateY(0)'})),
+      transition('void => *', [
+        style({transform: 'translateY(-200%)'}),
+        animate(1000)
+      ]),
+      transition('* => void', [
+        animate(1000, style({transform: 'translateY(-200%)'}))
+      ])
+    ]),
+    trigger('fadeInOut', [
+      state('in', style({opacity: 0.75})),
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(1000)
+      ]),
+      transition('* => void', [
+        animate(1000, style({opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class ThankYouModalComponent implements OnInit {
   private state;
@@ -19,6 +41,7 @@ export class ThankYouModalComponent implements OnInit {
     this.show = false;
     this.closed.emit(this.state);
   }
+
 
   constructor() { }
 
